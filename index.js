46bea15b7cd9e779bc6ac8b18540445b8a8d5612
Gabe-H <gabe.haarberg@gmail.com>
2020-09-20 23:49:15 -0700

    add auth

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express")
 const path = require('path')
+const SpotifyWebApi = require("spotify-web-api-node")
+require("dotenv").config()
 const PORT = process.env.PORT || 5000
+
+var spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: 'http://localhost:8080/callback'
+})
+
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('home'))
+    .get('/auth', (req, res) => {
+        var url = spotifyApi.createAuthorizeURL(['user-read-email'], '', true)
+        res.redirect(url)
+    })
     .listen(PORT, () => console.log(`listening on ${ PORT }`))
